467eb9d41d9c3849b1df682f145ed116e0e9654d
unknown <hyejuaya@gmail.com>
2025-07-29 11:03:54 -0300

    sobre java

---
+++ pw3-tarefas/src/main/java/br/com/etechas/tarefas/Pw3TarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechas.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw3TarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw3TarefasApplication.class, args);
+	}
+
+}
